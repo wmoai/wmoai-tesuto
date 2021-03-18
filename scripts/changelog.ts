c7f069ff6ed3b1af9430c2145c90222c9184d959
wmoai <wmoaism@gmail.com>
2021-03-18 13:34:36 +0900

    feat: add release flow

---
+++ scripts/changelog.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs'
+
+const stream = fs.createReadStream(`${__dirname}/../CHANGELOG.md`, {
+  encoding: 'utf8',
+  highWaterMark: 1024,
+})
+
+let buffer = ''
+
+stream.on('data', (chunk) => {
+  buffer += chunk.toString('utf8')
+  const matched = buffer.match(/#+\s\[?\d+\.\d+.\d+\]?/g)
+  if (matched === null || matched.length < 2) {
+    return
+  }
+  const prevHeader = matched[1]
+  const prevHeaderIndex = buffer.indexOf(prevHeader)
+  const currentChangelog = buffer.slice(0, prevHeaderIndex - 1)
+  console.log(currentChangelog)
+  stream.destroy()
+})
+
+stream.on('end', () => {
+  // This will only runs if two version headers are not found.
+  console.log(buffer)
+})
